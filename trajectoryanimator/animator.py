0c90a2fb05321220eced0c01643319a849b473d0
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2023-03-27 00:59:14 +0200

    some additional options and formatting

--- trajectoryanimator/animator.py
+++ trajectoryanimator/animator.py
@@ -6,23 +6,40 @@ import os.path
 from tqdm import tqdm
 from typing import List
 from datetime import datetime
+from matplotlib.collections import LineCollection
 
 AU = 149.6e9
 
 
 class TrajectoryParticle:
-    def __init__(self, name, dataFile:str, color:str, customData=None) -> None:
+    defaultcolor = "#F9F8F8"
+
+    def __init__(
+        self,
+        name,
+        dataFile: str,
+        color: str,
+        customData=None,
+        colorHistory=True,
+        tracerOn=True,
+    ) -> None:
         self.name = name
         self.color = color
+        self.tracerOn = tracerOn
+        # self.colorHistory = colorHistory
+        if colorHistory:
+            self.histColor = color
+        else:
+            self.histColor = TrajectoryParticle.defaultcolor
 
         if customData is None:
             data = self._readDataFile(dataFile)
         else:
             data = customData
-        
+
         self.timeData = data.loc[:, ["t"]]
         self.posData = data.loc[:, ["x", "y", "z"]]
-        
+
         self.startTime = self.timeData.t.iloc[0]
         self.endTime = self.timeData.t.iloc[-1]
 
@@ -30,8 +47,10 @@ class TrajectoryParticle:
         self.tracer = None
         # print(self.posData)
 
-
-    def _readDataFile(self, dataFile:str,):
+    def _readDataFile(
+        self,
+        dataFile: str,
+    ):
         if dataFile[-3:] == "dat":
             delimiter = "   "
             delimiter = "\t"
@@ -48,8 +67,8 @@ class TrajectoryParticle:
         ).drop(["vx", "vy", "vz"], axis=1)
 
         return data
-    
-    def _dropBySpeed(self, speed:int):
+
+    def _dropBySpeed(self, speed: int):
         self.timeData = self.timeData.iloc[::speed]
         self.posData = self.posData.iloc[::speed]
 
@@ -57,71 +76,88 @@ class TrajectoryParticle:
         # print(time, self.startTime, self.endTime)
         if (time >= self.startTime) and (time <= self.endTime):
             idx = self.timeData.query("t < @time").index
-            
+
             temp = self.posData.loc[idx]
             tempTracer = self.posData.loc[idx[-8::]]
-            
-            self.line.set_data(temp.x, temp.y) #, color=self.color)
+
+            self.line.set_data(temp.x, temp.y)  # , color=self.color)
             self.line.set_3d_properties(temp.z)
-            self.line.set_color(self.color)
-            self.line.set_alpha(0.2)
-            self.tracer.set_linewidth(0.7)
+            self.line.set_color(self.histColor)
+            self.line.set_alpha(1)
+            # self.line.set_linewidth(0.7)
+            if self.tracerOn:
+                self.tracer.set_data(tempTracer.x, tempTracer.y)  # , color=self.color)
+                self.tracer.set_3d_properties(tempTracer.z)
+                self.tracer.set_color(self.color)
+                self.tracer.set_linewidth(1.4)
+                
+                self.line.set_alpha(0.2)
 
-            self.tracer.set_data(tempTracer.x, tempTracer.y) #, color=self.color)
-            self.tracer.set_3d_properties(tempTracer.z)
-            self.tracer.set_color(self.color)
-            self.tracer.set_linewidth(1.4)
-        
-        if (time > self.endTime):
-            idx = self.timeData.query("t < @time").index
-            tempTracer = self.posData.loc[idx[-2::]]
-            self.tracer.set_data(tempTracer.x, tempTracer.y) #, color=self.color)
-            self.tracer.set_3d_properties(tempTracer.z)
-            # if time-self.startTime > (self.endTime-self.startTime)/2:
-            #     self.line.set_color(self.color)
-        
-    
+        if time > self.endTime:
+            if self.tracerOn:
+                idx = self.timeData.query("t < @time").index
+                tempTracer = self.posData.loc[idx[-2::]]
+                self.tracer.set_data(tempTracer.x, tempTracer.y)  # , color=self.color)
+                self.tracer.set_3d_properties(tempTracer.z)
+                # if time-self.startTime > (self.endTime-self.startTime)/2:
+                #     self.line.set_color(self.color)
 
-class TrajectoryAnimator():
-    def __init__(self, particles:List[TrajectoryParticle], plotLimits=AU, dpi=96, dt=3600, speed=50) -> None:
+
+class TrajectoryAnimator:
+    def __init__(
+        self,
+        particles: List[TrajectoryParticle],
+        plotLimits=AU,
+        dpi=96,
+        dt=3600,
+        speed=50,
+    ) -> None:
         plt.style.use("dark_background")
-
 
         self.plotLimits = plotLimits
 
         self.dpi = dpi
-        self.fig = plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
-        
+        self.fig = plt.figure(figsize=(1920 / dpi, 1080 / dpi), dpi=dpi)
+
         self.ax = self.fig.add_subplot(111, projection="3d")
 
         self.ax.set_ylim(-plotLimits, plotLimits)
         self.ax.set_xlim(-plotLimits, plotLimits)
-        self.ax.set_zlim(-plotLimits/2, plotLimits/2)
-
-
+        self.ax.set_zlim(-plotLimits / 2, plotLimits / 2)
 
         self.particles = particles
-        
+
         endTime = -1000
         startTime = 1e20
-        leftText = r""
-        for idx, particle in enumerate(self.particles):
-            particle.line = self.ax.plot([], [], [], clip_on=False)[0] # type: ignore
-            particle.tracer = self.ax.plot([], [], [], markevery=[-1], marker="o", clip_on=False)[0] # type: ignore
 
-            self.fig.text(x = 0.07, y=0.9-(0.02*idx), s=particle.name, color=particle.color, fontsize=14)
+        for idx, particle in enumerate(self.particles):
+            if particle.tracerOn:
+                particle.line = self.ax.plot([], [], [], clip_on=False)[0]  # type: ignore
+                particle.tracer = self.ax.plot([], [], [], markevery=[-1], marker="o", clip_on=False)[0]  # type: ignore
+            else:
+                particle.line = self.ax.plot([], [], [], marker="o", markevery=[-1], clip_on=False)[0]  # type: ignore
+
+            self.fig.text(
+                x=0.07,
+                y=0.9 - (0.02 * idx),
+                s=particle.name,
+                color=particle.color,
+                fontsize=14,
+            )
 
             particle._dropBySpeed(speed)
-            
+
             startTime = min(startTime, particle.timeData.t.iloc[0])
             endTime = max(endTime, particle.timeData.t.iloc[-1])
 
         self.lines = [particle.line for particle in self.particles]
 
-        self.timeData = np.arange(startTime, endTime, int(dt*speed))
+        self.timeData = np.arange(startTime, endTime, int(dt * speed))
 
         J2000inUnix = 946684800
-        self.dates = pd.to_datetime(self.timeData+J2000inUnix, unit="s").strftime("%Y-%m-%d")
+        self.dates = pd.to_datetime(self.timeData + J2000inUnix, unit="s").strftime(
+            "%Y-%m-%d"
+        )
 
         self.totalSteps = len(self.timeData)
         self.fig.tight_layout()
@@ -133,30 +169,49 @@ class TrajectoryAnimator():
 
 
     def _animateFunction(self, i):
-        self.ax.view_init(elev=15., azim=-130)
+        self.ax.view_init(elev=15.0, azim=-130)
         self.ax.set_title(self.dates[i], y=0.983, fontsize=14)
 
         for particle in self.particles:
             particle._updateLine(self.timeData[i])
         return self.lines
-    
+
+
     def runAnimation(self, savefile="anim", fps=120):
         fullSaveName = "animations/" + savefile + ".mp4"
         print("Saving to " + fullSaveName)
-        anim = FuncAnimation(self.fig, self._animateFunction, frames=tqdm(range(self.totalSteps)), interval=1, blit=True)
+
+        anim = FuncAnimation(
+            self.fig,
+            self._animateFunction,
+            frames=tqdm(range(self.totalSteps)),
+            interval=1,
+            blit=True,
+        )
 
         if not os.path.exists("animations"):
             os.makedirs("animations")
-        
-        anim.save(fullSaveName, fps=fps, extra_args=["-vcodec", "libx264"], dpi=self.dpi)
 
-
+        anim.save(
+            fullSaveName, fps=fps, extra_args=["-vcodec", "libx264"], dpi=self.dpi
+        )
 
 
 if __name__ == "__main__":
-    thing1 = TrajectoryParticle("Solar Sail 1 | Mass = 400 | Area = 12000", r"data/mass400_area12000.dat", "#d1495b")
-    thing2 = TrajectoryParticle("Solar Sail 2 | Mass = 700 | Area = 10000", r"data/mass700_area10000.dat", "#26C485")
+    thing1 = TrajectoryParticle(
+        "Solar Sail 1 | Mass = 400 | Area = 12000",
+        r"data/mass400_area12000.dat",
+        "#d1495b",
+        tracerOn=False
+    )
+    thing2 = TrajectoryParticle(
+        "Solar Sail 2 | Mass = 700 | Area = 10000",
+        r"data/mass700_area10000.dat",
+        "#26C485",
+        tracerOn=True,
+        colorHistory=False
+    )
 
-    traj = TrajectoryAnimator([thing1, thing2], speed=40)
+    traj = TrajectoryAnimator([thing1, thing2], speed=400)
 
     traj.runAnimation()
