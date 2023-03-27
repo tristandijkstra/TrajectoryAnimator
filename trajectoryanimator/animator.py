48d8c9713bc6f7407e6cbb0194e11a8c59364723
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2023-03-27 09:13:14 +0200

    a start to the camera sequence

--- trajectoryanimator/animator.py
+++ trajectoryanimator/animator.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os.path
 from tqdm import tqdm
-from typing import List
+from typing import List, Union
 from datetime import datetime
 from matplotlib.collections import LineCollection
 
@@ -90,7 +90,7 @@ class TrajectoryParticle:
                 self.tracer.set_3d_properties(tempTracer.z)
                 self.tracer.set_color(self.color)
                 self.tracer.set_linewidth(1.4)
-                
+
                 self.line.set_alpha(0.2)
 
         if time > self.endTime:
@@ -111,6 +111,8 @@ class TrajectoryAnimator:
         dpi=96,
         dt=3600,
         speed=50,
+        # camera:Union[CameraSequence, None] = None
+        camera=None,
     ) -> None:
         plt.style.use("dark_background")
 
@@ -167,7 +169,6 @@ class TrajectoryAnimator:
         self.ax.set_axis_off()
         # plt.axis("off")
 
-
     def _animateFunction(self, i):
         self.ax.view_init(elev=15.0, azim=-130)
         self.ax.set_title(self.dates[i], y=0.983, fontsize=14)
@@ -175,7 +176,6 @@ class TrajectoryAnimator:
         for particle in self.particles:
             particle._updateLine(self.timeData[i])
         return self.lines
-
 
     def runAnimation(self, savefile="anim", fps=120):
         fullSaveName = "animations/" + savefile + ".mp4"
@@ -197,22 +197,51 @@ class TrajectoryAnimator:
         )
 
 
+class CameraSequence:
+    def __init__(self) -> None:
+        self.sequence = []
+        self.endTimes = []
+
+    def __str__(self) -> str:
+        res = "Camera Sequence: \n"
+        for idx, u in enumerate(self.sequence):
+            res += f"{idx} | {u[0]} | elev = {u[1][0]} | azi = {u[1][1]} | roll = {u[1][2]}\n"
+        return res
+
+    def addSegment(
+        self, toFrac: float, elevation: float, azimuth: float, roll: float = 0
+    ) -> None:
+        segment = [toFrac, [elevation, azimuth, roll]]
+        self.endTimes.append(toFrac)
+        self.sequence.append(segment)
+
+    def clearSequence(self) -> None:
+        self.sequence = []
+
+
 if __name__ == "__main__":
     thing1 = TrajectoryParticle(
         "Solar Sail 1 | Mass = 400 | Area = 12000",
         r"data/mass400_area12000.dat",
         "#d1495b",
         tracerOn=True,
-        colorHistory=True
+        colorHistory=True,
     )
     thing2 = TrajectoryParticle(
         "Solar Sail 2 | Mass = 700 | Area = 10000",
         r"data/mass700_area10000.dat",
         "#26C485",
         tracerOn=True,
-        colorHistory=True
+        colorHistory=True,
     )
 
+    camera = CameraSequence()
+    camera.addSegment(0, 90, -130)
+    camera.addSegment(0.5, 90, -130)
+    camera.addSegment(0.6, 15, -130)
+    camera.addSegment(1, 15, -130)
+
+    print(camera)
     traj = TrajectoryAnimator([thing1, thing2], speed=40)
 
     traj.runAnimation()
